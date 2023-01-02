b1dcf759e5fc590cafc9a2836452ec883a070df5
benjamin giudice <ben.giudice@gmail.com>
2023-01-03 01:18:33 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 fn main() {
     let x = [(1,2u32),(3,4),(5,6),(7isize,8)];
     println!("-> {:?}", &x[2..]);
-    let mut out = String::new();
-    let _f = std::fmt::write(&mut out, format_args!("heyy"));
+    let out = String::new();
     println!("{out:?}");
+    let _d = || {};
+    let _h = || 4;
 }
+
+fn _does_things(i: i32) -> u8 {
+    i as u8
+}
+
 
 
 
