84e6b052badd6c0c846c2e5642064f7327fa6931
Benjamin Giudice <ben.giudice@gmail.com>
2023-01-05 22:55:42 +0800

    implement binary search

--- src/ch1_rand.rs
+++ src/ch1_rand.rs
@@ -1,24 +1,62 @@
-use std::io::{Write, Stdout, Stdin};
+use std::io::Write;
+
 use rand::Rng;
 
 pub fn ran() {
-    
+    println!("Choose an upper limit: ");
+    let stdin = &std::io::stdin();
+    let max = &mut String::new();
+    let max = loop {
+        stdin.read_line(max).expect("bad max");
+        let max = &**max;
+        let max = max.trim().parse::<u64>();
+        match max {
+            Ok(res) => break res,
+            Err(_) => continue,
+        };
+    };
+    let mut remaining = f64::log2(max as f64).ceil() as u64;
 
-    print!("Enter a number: ");
-    let ref mut stdout: Stdout = std::io::stdout();
-    stdout.flush().expect("sheit");
+    let rand = &mut rand::thread_rng();
+    let rand = rand.gen_range(1..=max);
 
-    let ref mut guess: String = String::new();
-    let stdin: &Stdin = &*&mut std::io::stdin();
-    stdin.read_line(guess).expect("nogood");
-    
-    let parsed: u32 = guess.trim().parse().expect("parse failed");
-    println!("You entered: {}", parsed);
+    println!("Guesses remaining: {}", remaining);
 
-    let ref mut rand_gen = rand::thread_rng();
-    let ans = rand_gen.gen_range(1..=5);
-    println!("And here's a random number: {}", ans);
+    let mut h = max;
+    let mut l = 0u64;
 
+    loop {
+        print!("Enter a number: ");
+        let stdout = &mut std::io::stdout();
+        stdout.flush().expect("sheit");
 
+        let guess= &mut String::new();
+        stdin.read_line(guess).expect("nogood");
+        let guess = &**guess;
+        let guess = guess.trim();
+
+        let guess = if guess == "b" { Ok((h + l) / 2) } else { guess.parse::<u64>()};
+                
+        let guess = match guess {
+            Ok(ans) => {remaining -= 1; ans},
+            Err(_) => continue,
+        };
+
+        let guess = &guess;
+        let rand = &rand;
+        let ans = guess.cmp(rand);
+
+        let attempt = match ans {
+            std::cmp::Ordering::Greater => {h = *guess; "too big"},
+            std::cmp::Ordering::Equal => "right!",
+            std::cmp::Ordering::Less => {l = *guess; "too small"},
+        };
+
+        println!("Your guess of {} was {}. {} remaining.", guess, attempt, remaining);
+
+        if ans == std::cmp::Ordering::Equal { break; }
+
+        if remaining <= 0 { println!("No guesses remaining!"); return; }
+    }
 
 }
